be9ca59f93526f8205a5d450042fc9e4cb4fcfd7
Gabriela Gonzales <gonzales.villarroel.gabriela@gmail.com>
2023-10-04 01:23:35 -0400

    Added list of tasks in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:5127/api/Assignment")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.taskName}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
